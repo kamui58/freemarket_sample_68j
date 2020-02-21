339f2288cc977e7f5a34c29d4670d1ea95807a14
kamui58 <owlprotectthewillage.58@gmail.com>
2020-02-21 14:45:18 +0900

    payjpの必要情報の記載完了

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,13 @@ Rails.application.routes.draw do
   resources :items
   resources :purchases
   resources :addresses
-  resources :credit_cards
+
+  resources :credit_cards, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 
 end
